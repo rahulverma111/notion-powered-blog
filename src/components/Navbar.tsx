021431f054b886d8590e7a015695f22da51b80ad
ShreyDhyani <shrey@betalectic.com>
2025-05-23 22:15:46 +0530

    chore: added top navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,9 @@ export default function Navbar() {
   return (
     <div className="bg-brand-0 h-10 flex justify-between items-center px-3">
       <h1>Logo</h1>
-      <div>
-        <Link href={"/authors"} />
-        <Link href={"/"} />
+      <div className="flex gap-x-4">
+        <Link href={"/"}>Blogs</Link>
+        <Link href={"/authors"}>Authors</Link>
       </div>
     </div>
   );

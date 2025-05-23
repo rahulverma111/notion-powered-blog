04a6b607bbcd230ecb16aa47128331a8a25ff674
parambetalectic <paramveer@betalectic.com>
2025-05-23 18:38:14 +0530

    feat:Enhance blog page with dynamic slug and static params generation

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	output: "export",
-};
-// next.config.js
-module.exports = {
-	env: {
-		NOTION_API_KEY: process.env.NOTION_API_KEY,
-		NOTION_POSTS_DB_ID: process.env.NOTION_POSTS_DB_ID,
-	},
+  /* config options here */
+  output: "export",
+  env: {
+    NOTION_API_KEY: process.env.NOTION_API_KEY,
+    NOTION_POSTS_DB_ID: process.env.NOTION_POSTS_DB_ID,
+  },
 };
 
 export default nextConfig;
--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,7 +1,25 @@
-export default function Blog() {
+import { getPosts } from "@/lib/notion";
+
+export async function generateStaticParams() {
+  // eslint-disable-next-line
+  const posts: any = await getPosts({ pageSize: 10 });
+
+  // eslint-disable-next-line
+  return posts.posts.map((post: { slug: string }) => ({
+    slug: post.slug,
+  }));
+}
+// eslint-disable-next-line
+export default async function Blog({
+  params,
+}: {
+  // eslint-disable-next-line
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
   return (
     <>
-      <h2>This is single blog!!!</h2>
+      <h2>This is single blog!!! - {slug}</h2>
     </>
   );
 }

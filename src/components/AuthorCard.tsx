d0cd16dc86d432c3543506fcfa17ec353cfafc55
rahulverma111 <rahul@betalectic.com>
2025-05-23 18:12:05 +0530

    shadcn added

---
+++ src/components/AuthorCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Label } from "./ui/label";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+interface AuthorCardProps {
+  heading: string;
+  subheading: string;
+  imageUrl: string;
+  blogCount: number;
+}
+
+export default function AuthorCard({
+  heading = "Rahul verma",
+  subheading = "Writing is my passion which gives me wings to fly!",
+  imageUrl = null,
+  blogCount = 312,
+}: AuthorCardProps) {
+  return (
+    <div>
+      <Label htmlFor="heading">{heading}</Label>
+      <Label htmlFor="subheading">{heading}</Label>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback></AvatarFallback>
+      </Avatar>
+    </div>
+  );
+}

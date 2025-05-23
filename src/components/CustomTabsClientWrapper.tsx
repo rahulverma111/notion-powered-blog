d828423a462c016fd7c8e26e4c2a7aec5d76ea1b
Arihant Jain <arihant@betalectic.com>
2025-05-23 21:00:40 +0530

    feat:Added wrapper

--- src/components/CustomTabsClientWrapper.tsx
+++ src/components/CustomTabsClientWrapper.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import CustomTabs from "@/components/CustomTabs";
 import BlogPost from "@/components/BlogPost";
 import { Post } from "@/lib/types";
+import { HorizontalBorder } from "@/components/HorizontalBorder";
 
 interface Props {
 	posts: Post[];
@@ -28,18 +29,21 @@ export default function CustomTabsClientWrapper({ posts }: Props) {
 			{activeTab === 1 ? (
 				<h1 className="text-2xl font-bold">Writer</h1>
 			) : (
-				<div className="flex flex-col gap-y-8">
+				<div className="flex flex-col gap-y-5">
 					{posts.map((post: Post) => (
-						<Link key={post.id} href={`/blogs/${post.id}`} className="p-4">
+						<Link key={post.id} href={`/${post.id}`} className="p-4">
 							<BlogPost
 								title={post.title}
+								// description={post.excerpt}
 								description="Learn the principles and patterns to build scalable backend systems with real-world examples."
 								authorName="Rakshith"
 								authorAvatarUrl="/avatars/rakshith.jpg"
 								publishedDate="May 23, 2025"
 								imageUrl="https://fastly.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0"
 							/>
+							{posts.indexOf(post) !== posts.length - 1 && <HorizontalBorder />}
 						</Link>
+						// </div>
 					))}
 				</div>
 			)}

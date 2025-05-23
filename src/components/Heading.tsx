c377bb0f65ba85ff89e1e7779b151d6f5ee5835d
ShreyDhyani <shrey@betalectic.com>
2025-05-23 18:38:55 +0530

    chore: added Headings

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,16 +1,26 @@
+import React from "react";
 import { twMerge } from "tailwind-merge";
 import { Label } from "./ui/label";
 
-export default function Heading({
-  headerText,
-  styles,
-}: {
-  headerText: string;
+export interface HeadingProps {
+  children: React.ReactNode;
   styles?: string;
-}) {
+}
+
+const H1: React.FC<HeadingProps> = ({ styles, children }) => {
   return (
-    <Label className={twMerge("font-bold text-2xl", styles)}>
-      {headerText}
+    <Label className={twMerge("font-semibold text-lg md:text-2xl", styles)}>
+      {children}
     </Label>
   );
-}
+};
+
+const H2: React.FC<HeadingProps> = ({ styles, children }) => {
+  return (
+    <Label className={twMerge("font-semibold text-base md:text-xl", styles)}>
+      {children}
+    </Label>
+  );
+};
+
+export { H1, H2 };

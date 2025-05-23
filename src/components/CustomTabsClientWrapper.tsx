7623541a15dab3e5d53030066a2e24267662d5da
RakshithBeta <rakshith@betalectic.com>
2025-05-23 22:50:35 +0530

    added loader

--- src/components/CustomTabsClientWrapper.tsx
+++ src/components/CustomTabsClientWrapper.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { useState } from "react";
-import Link from "next/link";
+import { useRouter } from "next/navigation"; // or from "next/router" if < 13
 import CustomTabs from "@/components/CustomTabs";
 import BlogPost from "@/components/BlogPost";
 import { Post } from "@/lib/types";
 import { HorizontalBorder } from "@/components/HorizontalBorder";
+import Loader from "./Loader";
 
 interface Props {
   posts: Post[];
@@ -13,6 +14,13 @@ interface Props {
 
 export default function CustomTabsClientWrapper({ posts }: Props) {
   const [activeTab, setActiveTab] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handlePostClick = (id: string) => {
+    setLoading(true);
+    router.push(`/${id}`);
+  };
 
   return (
     <>
@@ -28,19 +36,21 @@ export default function CustomTabsClientWrapper({ posts }: Props) {
 
       {activeTab === 1 ? (
         <h1 className="text-2xl font-bold">Writer</h1>
+      ) : loading ? (
+        <Loader />
       ) : (
         <div className="flex flex-col">
           {posts.map((post: Post) => (
-            <Link key={post.id} href={`/${post.id}`} className="p-4">
+            <div
+              key={post.id}
+              className="p-4 cursor-pointer"
+              onClick={() => handlePostClick(post.id)}
+            >
               <BlogPost
                 title={post.title}
-                // description={post.excerpt}
                 description="Learn the principles and patterns to build scalable backend systems with real-world examples."
-                // authorName="Rakshith"
                 authorName={post.author?.name}
-                // authorAvatarUrl="/avatars/rakshith.jpg"
                 authorAvatarUrl={post.author?.image}
-                // publishedDate="May 23, 2025"
                 publishedDate={post.date.split("T")[0]}
                 imageUrl={
                   post.coverImage ??
@@ -48,8 +58,7 @@ export default function CustomTabsClientWrapper({ posts }: Props) {
                 }
               />
               {posts.indexOf(post) !== posts.length - 1 && <HorizontalBorder />}
-            </Link>
-            // </div>
+            </div>
           ))}
         </div>
       )}

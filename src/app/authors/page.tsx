84724c7b245d3dddd83fe77c4f1207308f57fade
ShreyDhyani <shrey@betalectic.com>
2025-05-23 19:47:00 +0530

    chore: page change

--- src/app/authors/page.tsx
+++ src/app/authors/page.tsx
@@ -1,15 +1,5 @@
 import CustomTabs from "@/components/CustomTabs";
 
 export default function Authors() {
-  return (
-    <div className="">
-      <CustomTabs
-        outerWrapperClass={""}
-        tabsData={[
-          { title: "New-in", component: "" },
-          { title: "Writers", component: "" },
-        ]}
-      />
-    </div>
-  );
+  return <></>;
 }

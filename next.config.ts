a8dd1f26aa9afe0135779a7747c5d6e0dae7affa
parambetalectic <paramveer@betalectic.com>
2025-05-24 01:09:56 +0530

    feat: add NOTION_AUTHORS_DB_ID to environment variables in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	output: "export",
-	images: {
-		unoptimized: true,
-	},
-	env: {
-		NOTION_API_KEY: process.env.NOTION_API_KEY,
-		NOTION_POSTS_DB_ID: process.env.NOTION_POSTS_DB_ID,
-	},
+  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    NOTION_API_KEY: process.env.NOTION_API_KEY,
+    NOTION_POSTS_DB_ID: process.env.NOTION_POSTS_DB_ID,
+    NOTION_AUTHORS_DB_ID: process.env.NOTION_AUTHORS_DB_ID,
+  },
 };
 
 export default nextConfig;

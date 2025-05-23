d5b25db8182f4c53d82cc4aefdf2fc440e306a36
Aviweb <avi1273619@gmail.com>
2025-05-23 19:48:41 +0530

    details page

--- src/app/authors/details/page.tsx
+++ src/app/authors/details/page.tsx
@@ -1,6 +1,6 @@
-import AuthorCard from "@/components/AuthorCard";
-import Description from "@/components/Description";
-import Heading from "@/components/Heading";
+import { AuthorCardCompact } from "@/components/AuthorCard";
+import { D1 } from "@/components/Description";
+import { H1 } from "@/components/Heading";
 
 export default function Page() {
   const authorDetails = {
@@ -14,22 +14,40 @@ export default function Page() {
 
   return (
     <>
-      <div className="w-full flex flex-col items-center space-y-3 mt-8">
-        <div className="h-[200px] w-[200px] rounded-full overflow-hidden bg-gray-600">
-          <img
-            src="https://picsum.photos/id/237/200/300"
-            className="w-full h-full object-cover"
-            alt="Rounded"
-          />
-        </div>
+      <div className="max-w-[200px] sm:max-w-sm md:max-w-lg lg:max-w-4xl mx-auto">
+        <div className="w-full flex flex-col items-center space-y-3 mt-8">
+          <div className="h-[200px] w-[200px] rounded-full overflow-hidden bg-gray-600">
+            <img
+              src="https://picsum.photos/id/237/200/300"
+              className="w-full h-full object-cover"
+              alt="Rounded"
+            />
+          </div>
 
-        <Heading headerText={authorDetails?.authorName} styles="" />
-        <Description
-          descriptionText={authorDetails?.description}
-          styles="text-gray-600"
-        />
+          <H1 styles="">{authorDetails?.authorName}</H1>
+          <D1 styles="text-center">{authorDetails?.description}</D1>
+        </div>
         <HorizontalBorder />
-        <AuthorCard heading="Dummy" subheading="fgh" imageUrl="" />
+        <HorizontalBorder />
+
+        <div className="space-y-2 mt-10">
+          <H1>More Authors</H1>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 mt-8">
+            <AuthorCardCompact
+              heading="Dummy"
+              subheading="fgh"
+              imageUrl=""
+              blogCount={1}
+            />
+            <AuthorCardCompact
+              heading="Dummy"
+              subheading="fgh"
+              imageUrl=""
+              blogCount={1}
+            />
+          </div>
+        </div>
       </div>
     </>
   );

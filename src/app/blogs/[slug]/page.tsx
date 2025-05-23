90446f1f21a8fd54c00e03d3e47aa5cbb1bf9f33
parambetalectic <paramveer@betalectic.com>
2025-05-23 17:21:36 +0530

    feat: add static parameters generation for blog posts

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,3 +1,12 @@
+export async function generateStaticParams() {
+  // const posts = await fetch("https://.../posts").then((res) => res.json());
+  const posts = [{ slug: "233" }, { slug: "43" }];
+
+  return posts.map((post: { slug: string }) => ({
+    slug: post.slug,
+  }));
+}
+
 export default function Blog() {
   return (
     <>

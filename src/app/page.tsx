f2dde4b547ab91c3e391b4f1bfc49dbcf0787cbf
ShreyDhyani <shrey@betalectic.com>
2025-05-23 21:26:55 +0530

    chore: added Tab shift

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export const revalidate = 3600; // Revalidate every hour
 
 async function getStaticProps() {
   const { posts } = await getPosts({ pageSize: 10 });
-
   const { authors } = await getAuthors({ pageSize: 100 });
+
   return {
     props: {
       posts,
@@ -27,28 +27,33 @@ export default async function Home() {
       <CustomTabs
         outerWrapperClass={""}
         tabsData={[
-          { title: "New-in", component: "" },
+          { title: "New-in", component: <PostsComponent posts={posts} /> },
           { title: "Writers", component: "" },
         ]}
       />
-      {JSON.stringify(authors)}
-
-      <div className="flex flex-col gap-y-5">
-        {posts.map((post: Post) => (
-          <Link key={post.id} href={`/${post.id}`} className="p-4">
-            <BlogPost
-              title={post.title}
-              // description={post.excerpt}
-              description="Learn the principles and patterns to build scalable backend systems with real-world examples."
-              authorName="Rakshith"
-              authorAvatarUrl="/avatars/rakshith.jpg"
-              publishedDate="May 23, 2025"
-              imageUrl="https://fastly.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0"
-            />
-            {posts.indexOf(post) !== posts.length - 1 && <HorizontalBorder />}
-          </Link>
-        ))}
-      </div>
+      {/* {JSON.stringify(authors)}
+      {JSON.stringify(posts)} */}
     </main>
   );
 }
+
+const PostsComponent: React.FC<{ posts: Post[] }> = ({ posts }) => {
+  return (
+    <div className="flex flex-col gap-y-5">
+      {posts.map((post: Post) => (
+        <Link key={post.id} href={`/${post.id}`} className="p-4">
+          <BlogPost
+            title={post.title}
+            // description={post.excerpt}
+            description="Learn the principles and patterns to build scalable backend systems with real-world examples."
+            authorName="Rakshith"
+            authorAvatarUrl="/avatars/rakshith.jpg"
+            publishedDate="May 23, 2025"
+            imageUrl="https://fastly.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0"
+          />
+          {posts.indexOf(post) !== posts.length - 1 && <HorizontalBorder />}
+        </Link>
+      ))}
+    </div>
+  );
+};

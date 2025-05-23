9e00af39e9e27f4050c6e8bf774459013bea8c7c
Aviweb <avi1273619@gmail.com>
2025-05-23 20:53:17 +0530

    added author list

--- src/app/authors/page.tsx
+++ src/app/authors/page.tsx
@@ -1,3 +1,25 @@
-export default function Authors() {
-  return <></>;
+import { AuthorCardCompact } from "@/components/AuthorCard";
+
+export default async function AuthorsList() {
+  const data = [
+    {
+      authorName: "Rakshith",
+      imageUrl: "",
+    },
+    {
+      authorName: "Rashith",
+      imageUrl: "",
+    },
+  ];
+
+  return data.map((item) => {
+    return (
+      <AuthorCardCompact
+        heading={item.authorName}
+        subheading={"Subheading"}
+        imageUrl={item.imageUrl}
+        blogCount={1}
+      />
+    );
+  });
 }

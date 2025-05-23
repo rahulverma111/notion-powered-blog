98824830dd649a3a0154f7a7d90cde7d3ea78b9a
RakshithBeta <rakshith@betalectic.com>
2025-05-23 16:45:43 +0530

    added routes

---
+++ src/app/authors/page.tsx
@@ -0,0 +1,7 @@
+export default function Authors() {
+  return (
+    <>
+      <h2>This is Authours Page!!!</h2>
+    </>
+  );
+}
---
+++ src/app/blogs/[slug]/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <>
+      <h2>This is single blog!!!</h2>
+    </>
+  );
+}
